b2444c343034f78bc474508ad34babfb738143ee
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2025-06-22 14:09:31 -0600

    imagen anime

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import chicaAnime from './assets/anime.png'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    <img src={chicaAnime} className="anime" alt="Chica Anime" style={{width:'100px'}} />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
